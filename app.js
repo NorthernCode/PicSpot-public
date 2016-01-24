1df1f2135a45f7824bbab139fb487dcbbd42046c
Antti Virtanen <antti.ok.virtanen@gmail.com>
2016-01-24 23:48:16 +0200

    added upload path for testing

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello Express World!');
 });
 
+app.get('/upload', function (req, res) {
+  res.send('Upload page');
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
