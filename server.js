b5f80bfbede2be95a51b7717843f5cad788563c2
Antti Virtanen <antti.ok.virtanen@gmail.com>
2016-01-24 23:23:26 +0200

    testing without express mod

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello Express World!');
-});
-
-app.listen(1337, function () {
-  console.log('Example app listening on port 1337!');
-});
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Azure\n');
+}).listen(port);
