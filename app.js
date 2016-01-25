3cc88c87c5274af670df59a4b9b428ca60a4ca59
Antti Virtanen <antti.ok.virtanen@gmail.com>
2016-01-25 15:24:16 +0200

    file upload working

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 var express = require('express');
+var busboy = require('connect-busboy');
+var path = require('path');
+var filesystem = require('fs-extra');
 var app = express();
 
-app.get('/', function (req, res) {
+app.use(express.static('public'));
+app.use(busboy());
+
+
+app.get('/m', function (req, res) {
   res.send('Hello Express World!');
 });
 
-app.get('/upload', function (req, res) {
-  res.send('Upload page');
+app.route('/upload')
+  .post(function (req, res, next) {
+
+        var filestream;
+        req.pipe(req.busboy);
+        req.busboy.on('file', function (fieldname, file, filename) {
+            console.log("Uploading: " + filename);
+
+            //Path where image will be uploaded
+            filestream = filesystem.createWriteStream(__dirname + '/img/' + filename);
+            file.pipe(filestream);
+            filestream.on('close', function () {    
+                console.log("Upload Finished of " + filename);              
+                res.redirect('back');           //where to go next
+            });
+        });
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Listening on port 3000!');
 });
