02f1608036a5251c06614649ad7905201833d475
Antti Virtanen <antti.ok.virtanen@gmail.com>
2016-01-30 23:36:32 +0200

    azure db

--- bin/db.js
+++ bin/db.js
@@ -1,8 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
+  host     : 'eu-cdbr-azure-north-d.cloudapp.net',
+  user     : 'b957ca83399f7a',
   database : 'node'
+  password : '000fed76'
 });
 
 module.exports = connection;
