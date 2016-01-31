6ec221e36ec83859c64a5f9c8e7712637c3cabbb
Antti Virtanen <antti.ok.virtanen@gmail.com>
2016-01-31 22:52:32 +0200

    frontpage CSS done and image view CSS modified

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,13 @@
+//Current url to copt url text field
 $( '#copyUrl' ).val(window.location.href);
+
+//Copy to clipboard
 $( '#copyBtn' ).on( "click", function( event ) {
     $( '#copyUrl' ).select();
     document.execCommand("copy");
+});
+
+//Auto upload file
+$( '#fileUpload' ).change(function() {
+    $( '#uploadForm' ).submit();
 });
