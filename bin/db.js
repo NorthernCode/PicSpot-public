05f5e5b6ecfef115bd1f320f5cf37883ca6895d8
Antti Virtanen <antti.ok.virtanen@gmail.com>
2016-02-02 23:44:11 +0200

    attemp to fix syntax error on azure

--- bin/db.js
+++ bin/db.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host     : 'eu-cdbr-azure-north-d.cloudapp.net',
   user     : 'b957ca83399f7a',
-  database : 'PicSpot'
+  database : 'PicSpot',
   password : '000fed76'
 });
 
