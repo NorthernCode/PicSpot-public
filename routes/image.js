691933bfb7f80b6c0a6a8b7cd80d5961cf315eb7
Antti Virtanen <antti.ok.virtanen@gmail.com>
2016-02-02 23:38:31 +0200

    attemp to fix syntax error on azure

--- routes/image.js
+++ routes/image.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var mysql = require('mysql');
-var db = require('../bin/db');
+var database = require('./bin/db');
 var router = express.Router();
 
 /* GET home page. */
@@ -8,7 +8,7 @@ router.get('/:img', function(req, res, next) {
     var path = require('path');
     path.exists('./public/images/' + req.params.img, function(exists) { 
       if (exists) { 
-        db.query('SELECT name, comment FROM picspot_comments WHERE url="' + req.params.img + '";', function(err, result) {
+        database.query('SELECT name, comment FROM picspot_comments WHERE url="' + req.params.img + '";', function(err, result) {
             if (err) throw err;
             res.render('image', { img: req.params.img, imgUrl: '../images/' + req.params.img, comments: result });
         });
@@ -24,11 +24,11 @@ router.get('/:img', function(req, res, next) {
 router.post('/:img', function(req, res, next) {
     console.log(req.body);
     if(req.body.name != '' && req.body.comment != ''){
-        db.query('INSERT INTO picspot_comments SET ?', req.body, function(err, result) {
+        database.query('INSERT INTO picspot_comments SET ?', req.body, function(err, result) {
             if (err) throw err;
         });
     }
-    db.query('SELECT name, comment FROM picspot_comments WHERE url="' + req.params.img + '";', function(err, result) {
+    database.query('SELECT name, comment FROM picspot_comments WHERE url="' + req.params.img + '";', function(err, result) {
         if (err) throw err;
         console.log(result);
         res.render('image', { img: req.params.img, imgUrl: '../images/' + req.params.img, comments: result });
