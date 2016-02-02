9996f18d4712ff8012abbeb65809f827a7d2e431
Antti Virtanen <antti.ok.virtanen@gmail.com>
2016-02-02 21:41:28 +0200

    return 404 if requested image is not found on server

--- routes/image.js
+++ routes/image.js
@@ -6,10 +6,21 @@ var db = require('../bin/db');
 
 /* GET home page. */
 router.get('/:img', function(req, res, next) {
-    db.query('SELECT name, comment FROM picspot_comments WHERE url="' + req.params.img + '";', function(err, result) {
-        if (err) throw err;
-        res.render('image', { img: req.params.img, imgUrl: '../images/' + req.params.img, comments: result });
-    });
+    var path = require('path'); 
+    console.log(path.basename());
+    path.exists('./public/images/' + req.params.img, function(exists) { 
+      if (exists) { 
+        db.query('SELECT name, comment FROM picspot_comments WHERE url="' + req.params.img + '";', function(err, result) {
+            if (err) throw err;
+            res.render('image', { img: req.params.img, imgUrl: '../images/' + req.params.img, comments: result });
+        });
+      }else{
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+      }
+    }); 
+    
 });
 
 router.post('/:img', function(req, res, next) {
